df76663c8bad0029e5a3c354d75a24360edce7a8
Holden Maudlin <holdenmaudlin@Holdens-MacBook-Pro.local>
2019-06-12 17:49:38 -0700

    More templates

--- mp/lease/views.py
+++ mp/lease/views.py
@@ -8,22 +8,22 @@ from django.db.models import Q
 
 
 def index(request):
-    all_for_sales = Lease.objects.all()
-    recent_sales = Lease.objects.filter(completed=True).order_by('-id')
+    all_curr_leases = Lease.objects.filter(completed=False)
+    recent_leases = Lease.objects.filter(completed=True).order_by('-id')
     context = {
         'sale': False,
         'page': 'Lease',
-        'all_for_sales': all_for_sales,
-        'recent_sales': recent_sales,
+        'current_properties': all_curr_leases,
+        'recent_properties': recent_leases,
     }
     return render(request, 'sale/index.html', context)
 
 
 def leases(request, Lease_id):
     try:
-        req_sale = Lease.objects.get(pk=Lease_id)
+        req_sale = Lease.obects.get(pk=Lease_id)
     except Lease.DoesNotExist:
-        raise Http404("Listing does not exist.")
+        raise Http404("Lease listing does not exist.")
     context = {
         'sale': req_sale,
     }
--- mp/sale/views.py
+++ mp/sale/views.py
@@ -8,24 +8,25 @@ from django.db.models import Q
  
 
 def index(request):
-    all_for_sales = Sale.objects.all()
+    all_curr_sales = Sale.objects.filter(completed=False).filter(in_escrow=False)
     all_in_escrows = Sale.objects.filter(in_escrow=True)
     recent_sales = Sale.objects.filter(completed=True).order_by('-id')
     context = {
         'sale': True,
         'page': 'Sale',
-        'all_for_sales': all_for_sales,
-        'all_in_escrows':  all_in_escrows,
-        'recent_sales': recent_sales,
+        'current_properties': all_curr_sales,
+        'escrow_properties': all_in_escrows,
+        'recent_properties': recent_sales,
     }
     return render(request, 'sale/index.html', context)
+
 
 
 def sales(request, Sale_id):
     try:
         req_sale = Sale.objects.get(pk=Sale_id)
     except Sale.DoesNotExist:
-        raise Http404("Listing does not exist.")
+        raise Http404("Sale listing does not exist.")
     context = {
         'sale': req_sale, 
     }
