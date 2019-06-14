6c745eb96c4b4cef9ebe2390ee8dc76baf21e803
Holden Maudlin <holdenmaudlin@Holdens-MacBook-Pro.local>
2019-06-14 00:44:50 -0700

    migrations

--- home/management/commands/createsu.py
+++ home/management/commands/createsu.py
@@ -3,6 +3,6 @@ from django.contrib.auth.models import User
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        if not User.objects.filter(username="admin").exists():
+        if not User.objects.filter(username="holden.maudlin").exists():
             User.objects.create_superuser("holden.maudlin", "holden.maudlin@gmail.com", "dodgers13")
             self.stdout.write(self.style.SUCCESS('Successfully created new super user'))
