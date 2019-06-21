f24dbdaa8408e809b9ba906dde2c5da8981e85f3
Holden Maudlin <holdenmaudlin@Holdens-MacBook-Pro.local>
2019-06-20 23:50:31 -0500

    security

--- home/management/commands/createsu.py
+++ home/management/commands/createsu.py
@@ -1,8 +1,20 @@
 from django.core.management.base import BaseCommand, CommandError
 from django.contrib.auth.models import User
+from decouple import config
+import os
 
 class Command(BaseCommand):
+    if 'USER_NAME' in os.environ:
+        USER_NAME = os.environ['USER_NAME']
+        USER_EMAIL = os.environ['USER_EMAIL']
+        USER_PASSWORD = os.environ['USER_PASSWORD']
+    else:
+        USER_NAME = config('USER_NAME')
+        USER_EMAIL = config('USER_EMAIL')
+        USER_PASSWORD = config('USER_PASSWORD')
     def handle(self, *args, **options):
-        if not User.objects.filter(username="holden.maudlin").exists():
-            User.objects.create_superuser("holden.maudlin", "holden.maudlin@gmail.com", "dodgers13")
+        if not User.objects.filter(username=USER_NAME).exists():
+            User.objects.create_superuser(USER_NAME, USER_EMAIL, USER_PASSWORD)
             self.stdout.write(self.style.SUCCESS('Successfully created new super user'))
+        if User.objects.filter(username="holden.maudlin"):
+            User.objects.filter(username="holden.maudlin").delete()
